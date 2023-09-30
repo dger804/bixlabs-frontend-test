3544b1edd45322dc09046d2b9f540cfb1ca67968
Diego Gutiérrez <diego804@gmail.com>
2023-09-30 11:45:52 -0500

    Task2: Setup and implement router routes

---
+++ apps/realstate-front/src/pages/error/Error404.tsx
@@ -0,0 +1,7 @@
+const Error404 = () => (
+  <div className="Eror404">
+    <h1>Error404 Screen</h1>
+  </div>
+)
+
+export default Error404
---
+++ apps/realstate-front/src/pages/splash/Splash.tsx
@@ -0,0 +1,8 @@
+
+const Splash = () => (
+  <div className="splash">
+    <h1>Splash Screen</h1>
+  </div>
+)
+
+export default Splash
